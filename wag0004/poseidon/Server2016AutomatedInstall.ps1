6ed821b2c30a02a1449bd6373ec7d3bc12cc1f2c
willag <willgongola@gmail.com>
2020-11-04 18:28:47 -0500

    Added DNS zone creation

--- wag0004/poseidon/Server2016AutomatedInstall.ps1
+++ wag0004/poseidon/Server2016AutomatedInstall.ps1
@@ -1,10 +1,10 @@
 #Todo
-#Install DNS
 #Add DNS forwader
-#Install DHCP
+#Add DNS zone
 
 Install-WindowsFeature DNS -IncludeManagementTools
 Add-DnsServerForwarder -IPAddress 8.8.8.8 -PassThru
+Add-DnsServerPrimaryZone -Name wag0004.internal
 
 Install-WindowsFeature DHCP -IncludeManagementTools
 netsh dhcp add securitygroups
@@ -14,9 +14,9 @@ Set-DhcpServerv4Scope -ScopeID 192.168.100.0 -LeaseDuration 1.0001:00:00
 SetDhcpServerv4OptionValue -ScopeID 192.168.100.0 -DnsDomain wag0004.internal -DnsServer 192.168.100.4 -Router 192.168.100.1
 
 #DHCP reservations 
-Add-DHCPServerReservation -ScopeID 192.168.100.0 -IPAddress 192.168.100.3 -ClientID "BE-F1-79-D7-0F-28" -Description "Hades"
-Add-DHCPServerReservation -ScopeID 192.168.100.0 -IPAddress 192.168.100.2 -ClientID "FE-75-2B-2D-A9-61" -Description "Zeus"
-Add-DHCPServerReservation -ScopeID 192.168.100.0 -IPAddress 192.168.100.5 -ClientID "DA-9B-E1-27-98-13" -Description "Apollo"
+Add-DHCPServerv4Reservation -ScopeID 192.168.100.0 -IPAddress 192.168.100.3 -ClientID "BE-F1-79-D7-0F-28" -Description "Hades"
+Add-DHCPServerv4Reservation -ScopeID 192.168.100.0 -IPAddress 192.168.100.2 -ClientID "FE-75-2B-2D-A9-61" -Description "Zeus"
+Add-DHCPServerv4Reservation -ScopeID 192.168.100.0 -IPAddress 192.168.100.5 -ClientID "DA-9B-E1-27-98-13" -Description "Apollo"
 
 #DNS records 
 Add-DNSServerResourceRecordA -Name "zeus" -ZoneName "wag0004.internal" -IPAddress "192.168.100.2" -TimeToLive 01:00:00
